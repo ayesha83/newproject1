e6c0b07ec633c1051e319752f1177b7e23a19e65
ayesha83 <60700582+ayesha83@users.noreply.github.com>
2020-02-05 14:08:57 +0000

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,43 @@
+// let coffeeOrder = ["Liam - Cappuccino", "Leona - Cortado", "Charlie - Whatever's new"]
+
+// console.log(coffeeOrder)
+
+
+// let songChoice = ["maroon5 - 22", "bsb - 23", "coldplay - 24"]
+// console.log(songChoice)
+// console.log(songChoice[2])
+
+// songChoice[0] = "new - new"
+// console.log(songChoice[0])
+// console.log(songChoice.length)
+// songChoice.push('latest - latest')
+// console.log(songChoice)
+// songChoice.pop()
+// console.log(songChoice)
+
+// let websiteList = ["bbc.com","starfall.com","cbbc.com"]
+// websiteList.push("dawn.com", "geo.tv")
+// console.log(websiteList)
+// websiteList.pop()
+// console.log(websiteList)
+
+// let websiteList = ["bbc.com","starfall.com","cbbc.com"]
+// websiteList.shift()
+// console.log(websiteList)
+
+// let websiteList = ["bbc.com","starfall.com","cbbc.com"]
+// websiteList.unshift("dailymail.com")
+// console.log(websiteList)
+
+// let websiteList = ["bbc.com","starfall.com","cbbc.com"]
+// console.log(websiteList.slice(0,1))
+//console.log(new)
+
+// let websiteList = ["bbc.com","starfall.com","cbbc.com"]
+// let pos = 1
+// let n = 1
+// let removedItem = websiteList.splice(0,1) 
+// console.log(removedItem)
+
+let websiteList = ["bbc.com","starfall.com","cbbc.com"]
+console.log(websiteList.indexOf("starfall.com"))
